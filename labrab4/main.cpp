1bdc37ad44d3bf4b72cbd42a4322729cca289c6e
user <user@ubuntu.ubuntu-domain>
2022-04-07 10:41:36 +0300

    added comments for lr4

--- labrab4/main.cpp
+++ labrab4/main.cpp
@@ -5,18 +5,23 @@ using namespace std;
 
 int main() {
 
+    //types of comments
     const unsigned int COMMENT_NONE = 0;
     const unsigned int COMMENT_LINE = 1;
     const unsigned int COMMENT_BLOCK = 2;
 
+    //file input
     fstream input;
     input.open("input", ios::in);
 
     string currentLine;
     unsigned int commented = COMMENT_NONE;
+    //read line
     while (getline(input, currentLine)) {
+        //parsing line
         for (int i=0; i<currentLine.size(); i++) {
 
+            // comment parser for // and /*
             if (currentLine[i] == '/' && i+1 < currentLine.size()) {
                 if (currentLine[i+1] == '/') {
                     commented = COMMENT_LINE;
@@ -26,6 +31,7 @@ int main() {
                 }
             }
 
+            //comment parser for */
             if (i-2 >= 0 && (currentLine[i-1] == '/' && currentLine[i-2] == '*')) {
                 if (i-3 >= 0) {
                     if (currentLine[i-2] != '/') {
@@ -37,15 +43,18 @@ int main() {
                 }
             }
 
+            //if no comment
             if (!commented) {
                 cout << currentLine[i];
             }
         }
 
+        //logic for // when line ends
         if (commented == 1) {
             commented = COMMENT_NONE;
         }
 
+        //new line if no comment
         if (!commented) cout << endl;
     }
 
